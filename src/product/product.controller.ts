d6bec55565284921c9168b6ccd3c10ccbaa520b6
Impalatore <naim7710@gmail.com>
2022-11-13 11:07:38 -0500

    Actualizaciones

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,15 +1,13 @@
 import { Body, Controller, HttpStatus, Post, Res } from "@nestjs/common";
 import { CreateProductoDTO } from "./dto/product.dto";
-import { json } from 'express';
-import { log } from "util";
 
 @Controller('product')
-export class ProductController {
-  @Post('/create')
+export class ProductoController {
+  @Post('/api/create')
   createPost(@Res() res, @Body() createProductoDTO: CreateProductoDTO) {
     console.log(createProductoDTO);
     res.status(HttpStatus.OK).json({
-      message: 'Recibido',
-    });
+      message: 'Producto recibido',
+    })
   }
 }
